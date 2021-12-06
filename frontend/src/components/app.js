4b4369f83190044f29b3f385cea92335e3a740cd
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-12-06 13:35:25 -0800

    styling update

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -8,7 +8,8 @@ import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
-
+import './reset.css'
+import './app.css'
 
 const App = () => (
   <div>
@@ -16,9 +17,7 @@ const App = () => (
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
-      <AuthRoute exact path="/signup" component={SignupFormContainer} />
-
-      
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />      
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
      
     </Switch>
--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import './main_page.css'
 
 class MainPage extends React.Component {
 
   render() {
     return (
       <div>
-        <p>GameDart</p>
+        <div className="splash-nav">
+          <Link>Sign Up</Link>
+          <Link>Log In</Link>
+        </div>
+        <div className="splash-title">
+          <div className="splash-container">
+            <h1>GameDart</h1>
+            <h3>Manage your neverending backlog of video games.</h3>
+          </div>
+        </div>
+
+        <div className="splash-desc">
+          <h3>Discover new games to potentially never play.</h3>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eros in cursus turpis massa tincidunt dui ut ornare. Erat velit scelerisque in dictum non consectetur. Dui ut ornare lectus sit amet est. Purus in massa tempor nec. Vitae suscipit tellus mauris a. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Mattis nunc sed blandit libero. Et malesuada fames ac turpis egestas integer. Cursus turpis massa tincidunt dui ut ornare lectus. Vitae et leo duis ut diam quam nulla. Eget dolor morbi non arcu risus quis varius quam quisque. Platea dictumst quisque sagittis purus sit amet. Rutrum quisque non tellus orci ac auctor. A scelerisque purus semper eget duis.
+          </div>
+        </div>
+
         <footer>
           Made by Team Culture
         </footer>
