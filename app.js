0887e47e34af1b20a6d01dc229e7ec398e65b4b8
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-12-07 22:54:09 -0800

    heroku push v2

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const users = require('./routes/api/users')
 const gameseeds = require('./routes/api/game_seed')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
