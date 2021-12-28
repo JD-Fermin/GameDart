52d0809ac1216ef1ae2c1e1429c9a055783f1961
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-12-28 01:38:59 -0800

    Added patch route for reviews

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -26,7 +26,7 @@ router.post('/',
                 gameId: req.body.gameId,
                 author: req.body.author,
                 body: req.body.body,
-                rating: req.body.rating
+                rating: parseInt(req.body.rating)
               });
               newReview.save()
               .then ((cur) => {
@@ -39,4 +39,39 @@ router.post('/',
     .catch(errs => res.status(400).json(errs))
 }); 
 
+router.patch('/:id',
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateReviewInput(req.body);
+    if(!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    User.findOne({_id: req.body.author})
+      .then( async (user) => {
+        if (!user) {
+          return res.status(400).json({ author: "The author does not exist" });
+        } else {
+
+              let updatedReview = Review.findByIdAndUpdate(req.params.id, {$set:{
+                body: req.body.body,
+                rating: parseInt(req.body.rating)
+              }}, (err, docs) => {
+                if (err) {
+                  console.log(err)
+                }
+
+                res.send({
+                  gameId: req.body.gameId,
+                  author: req.body.author,
+                  body: req.body.body,
+                  rating: parseInt(req.body.rating)
+                })
+              })
+        }
+      })
+    .catch(errs => res.status(400).json(errs))
+
+  }
+)
 module.exports = router;
