84ce6db212f5b0b57f78dd45ac17473343a58a6b
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-12-28 23:44:19 -0800

    added components

---
+++ frontend/src/components/backlog/backlog_index_container.js
@@ -0,0 +1,33 @@
+import BacklogIndex from "./backlog_index";
+import { 
+  updatePlayedGames, 
+  deleteBackLogGames,
+  fetchUserInfo,
+  updateBackLogGames,
+  deletePlayedGames
+} from "../../actions/user_actions";
+import { connect } from "react-redux";
+
+
+const mSTP = (state) => {
+  return {
+    currentId: state.session.user.id,
+    user: state.user[state.session.user.id]
+  };
+};
+
+const mDTP = (dispatch) => {
+  return {
+    fetch: userId => dispatch(fetchUserInfo(userId)),
+    delete: payload => dispatch(deleteBackLogGames(payload)),
+    setPlayed: payload => dispatch(updatePlayedGames(payload)),
+    setBackLog: payload => dispatch(updateBackLogGames(payload)),
+    deletePlayed: payload => dispatch(deletePlayedGames(payload))
+    // fetchGame: () => dispatch
+  }
+}
+
+export default connect(
+  mSTP,
+  mDTP
+)(BacklogIndex);
