8e643dfdcd804f176c9cdb4f75b97e7aac7237ea
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-12-30 15:05:44 -0500

    Begin edits for Developer page

--- frontend/src/components/gitHub/gitHub_container.js
+++ frontend/src/components/gitHub/gitHub_container.js
@@ -22,26 +22,27 @@ class GitHubLinks extends React.Component {
 
         <div className="the-gang">
           <div className="developers">
-            <img onClick={this.visitSite("https://github.com/hendricksimonr")} src="https://disneycinema.s3.us-east-2.amazonaws.com/Hendrick+Headshot+(2).JPEG" alt="" />
+            <img className="developer-photo" onClick={this.visitSite("https://github.com/hendricksimonr")} src="https://disneycinema.s3.us-east-2.amazonaws.com/Hendrick+Headshot+(2).JPEG" alt="" />
             <h2>Hendrick Ramos</h2>
+            <img onClick={this.visitSite("https://github.com/hendricksimonr")} src="https://disneycinema.s3.us-east-2.amazonaws.com/Hendrick+Headshot+(2).JPEG" alt="" />
             <p onClick={this.visitSite("https://github.com/hendricksimonr")}>Hendrick's GitHub</p>
             {/* <p onClick={this.visitSite("https://github.com/hendricksimonr")}>Hendrick's GitHub</p> */}
           </div>
           
           <div className="developers">
-            <img onClick={this.visitSite("https://github.com/JD-Fermin")} src="https://disneycinema.s3.us-east-2.amazonaws.com/mern-gang/Daniel.jpeg" alt="" />
+            <img className="developer-photo"  onClick={this.visitSite("https://github.com/JD-Fermin")} src="https://disneycinema.s3.us-east-2.amazonaws.com/mern-gang/Daniel.jpeg" alt="" />
             <h2>Jan Daniel Fermin</h2>
             <p onClick={this.visitSite("https://github.com/JD-Fermin")}>Daniel's GitHub</p>
           </div>
 
           <div className="developers">
-            <img onClick={this.visitSite("https://github.com/jshin720")} src="https://disneycinema.s3.us-east-2.amazonaws.com/mern-gang/Jeff.jpeg" alt="" />
+            <img className="developer-photo"  onClick={this.visitSite("https://github.com/jshin720")} src="https://disneycinema.s3.us-east-2.amazonaws.com/mern-gang/Jeff.jpeg" alt="" />
             <h2>Jeff Shin</h2>
             <p onClick={this.visitSite("https://github.com/jshin720")}>Jeff's GitHub</p>
           </div>
 
           <div className="developers">
-            <img onClick={this.visitSite("https://github.com/kenthiroi")} src="https://disneycinema.s3.us-east-2.amazonaws.com/mern-gang/Kent.jpeg" alt="" />
+            <img className="developer-photo"  onClick={this.visitSite("https://github.com/kenthiroi")} src="https://disneycinema.s3.us-east-2.amazonaws.com/mern-gang/Kent.jpeg" alt="" />
             <h2>Kent Hiroi</h2>
             <p onClick={this.visitSite("https://github.com/kenthiroi")}>Kent's GitHub</p>
           </div>
